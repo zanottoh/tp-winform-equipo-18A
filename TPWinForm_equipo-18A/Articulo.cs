24f02d2e5923d8ffca4f541e66a416d54655d345
Matias4kd <matiasfourcade6@gmail.com>
2024-09-03 11:36:53 -0300

    Se crea clase articulo con sus propiedades

---
+++ TPWinForm_equipo-18A/Articulo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPWinForm_equipo_18A
+{
+    internal class Articulo
+    {
+        public string Nombre{ get; set; }
+        public string Descripcion { get; set; }
+        public Marca Marca{ get; set; }
+        public Categoria Categoria { get; set; }
+        public float Precio{ get; set; }
+    }
+}
